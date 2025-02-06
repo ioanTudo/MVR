937601b726184e34840ddb76499501297cd6b7fb
Ioan Tudor <ioantudor@Laptop-Ioan.local>
2025-02-06 23:51:36 +0200

    ready to deliver

--- src/components/footer/FooterDisplay.jsx
+++ src/components/footer/FooterDisplay.jsx
@@ -1,9 +1,19 @@
+import { Link } from "react-router";
 import "./Footer.css";
 
 export const FooterDisplay = () => {
   return (
-    <footer style={{ borderTop: "1px solid black", textAlign: "center" }}>
-      <h1>footer</h1>
+    <footer className="footer">
+      <div className="footer_container">
+        <p>&copy; {new Date().getFullYear()} MovieRate. All rights reserved.</p>
+        <nav>
+          <Link aria-disabled href="/privacy-policy">
+            Privacy Policy
+          </Link>
+          |<Link href="/terms-of-service">Terms of Service</Link> |
+          <Link href="/contact">Contact</Link>
+        </nav>
+      </div>
     </footer>
   );
 };
